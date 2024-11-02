7c8f6e2a3f2a006bca61455e9e8cd4fe62868657
Z. B. Cirit <zbcirit@gmail.com>
2024-11-02 21:21:37 +0300

    Added basic API functionality

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
-
+videos = {"1":160, "2":120, "3":140}
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    print("hello")
+    return render_template("root.html")
+
+@app.route("/<video_id>")
+def video_request(video_id):
+    if video_id  not in videos:
+        abort(404, description = "Video id not found")
+    else:
+        return {
+            "id": video_id,
+            "BPM": videos[video_id]
+        }
